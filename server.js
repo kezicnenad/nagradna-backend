759653987eb184ea1b8b64b2ac92db5a29039bb2
Gluvi Ćuko <gluvaja@Moj-Mekic.local>
2024-10-15 01:38:22 +0200

    Bez spajanja na mongodb

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ app.get('/api', (req, res) => {
   res.send(`${MESSAGE} ${PORT}`);
 });
 
-// connect to mongoDB
-mongoose.connect(process.env.MDB_CONNECT, (err) => {
-  if (err) return console.error(err);
-  console.log('Connected to MongoDB');
-});
+// // connect to mongoDB
+// mongoose.connect(process.env.MDB_CONNECT, (err) => {
+//   if (err) return console.error(err);
+//   console.log('Connected to MongoDB');
+// });
 
 // set up routes
 app.use(express.json());
@@ -31,12 +31,12 @@ app.use(cors({
   credentials: true,
 }));
 
-app.use('/api/auth', require('./routers/userRouter'));
-app.use('/api/light', require('./routers/lightRouter'));
-app.use('/api/ventilation', require('./routers/ventilationRouter'));
-app.use('/api/temperature_humidity', require('./routers/temperatureHumidityRouter'));
-app.use('/api/soil_humidity', require('./routers/soilHumidityRouter'));
-app.use('/api/device', require('./routers/deviceRouter'));
+// app.use('/api/auth', require('./routers/userRouter'));
+// app.use('/api/light', require('./routers/lightRouter'));
+// app.use('/api/ventilation', require('./routers/ventilationRouter'));
+// app.use('/api/temperature_humidity', require('./routers/temperatureHumidityRouter'));
+// app.use('/api/soil_humidity', require('./routers/soilHumidityRouter'));
+// app.use('/api/device', require('./routers/deviceRouter'));
 
 // setup server
 app.listen(PORT, () => {
